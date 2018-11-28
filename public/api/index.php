4c190efed80b4b61bacda5b2cb24749069f6940d
Josh Gulledge <jgulledge19@outlook.com>
2018-11-28 16:39:59 -0500

    Fix pathing for api/index.php

--- public/api/index.php
+++ public/api/index.php
@@ -2,10 +2,11 @@
 use LCI\MODX\Slim\App;
 
 $autoload_possible_paths = [
-    // if cloned from git:
-    dirname(dirname(dirname(__DIR__))).'/vendor/autoload.php',
     // if installed via composer:
-    dirname(dirname(dirname(__DIR__))).'/core/vendor/autoload.php',
+    dirname(__DIR__).'/core/vendor/autoload.php',
+    dirname(dirname(__DIR__)).'/core/vendor/autoload.php',
+    // if cloned from git:
+    dirname(dirname(__DIR__)).'/vendor/autoload.php',
 ];
 foreach ($autoload_possible_paths as $autoload_path) {
     if (file_exists($autoload_path)) {
